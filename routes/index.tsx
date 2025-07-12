3292212d8e35b782c0793ebe698b38090b1dbb7a
Russel Dinoy <dinoyrussel@gmail.com>
2025-07-12 15:00:49 +0800

    new content added

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,9 +1,9 @@
 export default function Home() {
   return (
-    <main class="mx-auto p-8">
-      <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
+    <main class="mx-auto">
+      <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center px-8">
         <img
-          class="my-6 rounded-full"
+          class="my-6 rounded-full border-4 border-cyan-700"
           src="/russeldinoy-profile.webp"
           width="128"
           height="128"
@@ -24,10 +24,10 @@ export default function Home() {
         </p>
       </div>
 
-      <section class="max-w-screen-md mx-auto mt-8">
-        <h2 class="text-2xl">Projects</h2>
+      <section class="max-w-screen-md mx-auto mt-8 px-8">
+        <h2 class="text-2xl font-bold">Projects</h2>
         <hr class="my-2"></hr>
-        <div class="flex border my-4">
+        <div class="flex border my-4 rounded-lg">
           <div class="p-4">
             <h3 class="font-bold text-base">
               Realtour Boss - Real Estate Website
@@ -39,7 +39,7 @@ export default function Home() {
           </div>
         </div>
 
-        <div class="flex border my-4">
+        <div class="flex border my-4 rounded-lg">
           <div class="p-4">
             <h3 class="font-bold text-base">
               Realtour Boss - Real Estate Website
@@ -52,13 +52,35 @@ export default function Home() {
         </div>
       </section>
 
-      <section class="max-w-screen-md mx-auto mt-8">
-        <h2 class="text-2xl">About Me</h2>
+      <section class="max-w-screen-md mx-auto mt-8 px-8">
+        <h2 class="text-2xl font-bold">About Me</h2>
         <hr class="my-2"></hr>
+        <p class="mb-2">
+          Hi, I'm Russel, a passionate Full-stack developer dedicated to
+          crafting intuitive and engaging user experiences. My journey into web
+          development started with a fascination for how design and code come
+          together to create something beautiful and functional. I thrive on
+          bringing ideas to life, transforming complex concepts into clean,
+          efficient, and user-friendly web applications.
+        </p>
+        <p class="mb-2">
+          I specialize in React.js, JavaScript, HTML, and CSS, and I'm always
+          eager to learn new technologies that enhance the user experience. What
+          truly excites me is the collaborative process—working with designers
+          and back-end developers to solve challenges and build seamless digital
+          products that users love.
+        </p>
+        <p class="mb-2">
+          When I'm not coding, you can find me [mention a brief, relatable
+          hobby, e.g., "exploring new hiking trails," "experimenting with new
+          recipes," "diving into a good sci-fi novel"]. I'm currently seeking
+          opportunities to join a forward-thinking team where I can contribute
+          to impactful projects and continue to grow as a developer.
+        </p>
       </section>
 
-      <section class="max-w-screen-md mx-auto mt-8">
-        <h2 class="text-2xl">Skills and Technologies</h2>
+      <section class="max-w-screen-md mx-auto mt-8 px-8">
+        <h2 class="text-2xl font-bold">Skills and Technologies</h2>
         <hr class="my-2"></hr>
         <div class="mb-6">
           <div class="flex justify-between items-center mb-1">
@@ -141,8 +163,8 @@ export default function Home() {
         </div>
       </section>
 
-      <section class="max-w-screen-md mx-auto mt-8">
-        <h2 class="text-2xl">Contact</h2>
+      <section class="max-w-screen-md mx-auto mt-8 px-8">
+        <h2 class="text-2xl font-bold">Contact</h2>
         <hr class="my-2"></hr>
       </section>
 
